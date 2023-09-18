13054fc23ad8ffff347fa6fd2b8c4b0af52eb832
CompBioIPM <47293318+CompBioIPM@users.noreply.github.com>
2023-09-18 21:34:57 +0330

    Update inference_in_mouse_tissues.R

--- Fig.6/inference_in_mouse_tissues.R
+++ Fig.6/inference_in_mouse_tissues.R
@@ -1,24 +1,34 @@
-rm(list = ls())
 
-library(readr)
-library(ROCR)
-################################################################################
-setwd(paste0("D:\\scGATE\\scGATE\\R"))
-load("base_GRNs.RData")
+rm(list = ls())
+library(scGATE)
 ################################################################################
 tissue_of_interest    <- "Spleen-10X_P4_7"
 
 # load data
-tissue         <- strsplit(tissue_of_interest, "-")[[1]][1]
-target_names   <- unlist(read.table(paste0("D:\\scRNA logic\\mouse spleen dataset\\BEELINE\\data_cor\\", tissue_of_interest, "\\","tg_filtered.txt")))
-tf_names       <- unlist(read.table(paste0("D:\\scRNA logic\\mouse spleen dataset\\BEELINE\\data_cor\\", tissue_of_interest, "\\","tf_filtered.txt")))
+load("base_GRNs_mouse_tissues.RData")
+# load base grns for each tissue
+if(tissue=="Spleen"){
+  base_grn     <- atac_candidates_peak[[1]]
+}else if(tissue=="Lung"){
+  base_grn     <- atac_candidates_peak[[2]]
+}else if(tissue=="Liver"){
+  base_grn     <- atac_candidates_peak[[3]]
+}else if(tissue=="Kidney"){
+  base_grn     <- atac_candidates_peak[[4]]
+}else if(tissue=="Heart_and_Aorta"){
+  base_grn     <- atac_candidates_peak[[5]]
+}
 
-data           <- as.data.frame(read.csv(paste0("D:\\scRNA logic\\mouse spleen dataset\\BEELINE\\data_cor\\" , tissue_of_interest, "\\", "ExpressionData.csv") , header = TRUE))
+tissue         <- strsplit(tissue_of_interest, "-")[[1]][1]
+target_names   <- unlist(read.table(paste0("\\", tissue_of_interest, "\\","tg_filtered.txt")))
+tf_names       <- unlist(read.table(paste0("\\", tissue_of_interest, "\\","tf_filtered.txt")))
+
+data           <- as.data.frame(read.csv(paste0("\\" , tissue_of_interest, "\\", "ExpressionData.csv") , header = TRUE))
 gene_names     <- data[ ,1]
 data           <- t(data[ ,2:ncol(data)])
 colnames(data) <- gene_names
 
-# make sure tfs and target genes are present in the data
+# make sure tfs and target genes are present in the expression data
 tf_filtered    <- intersect(tf_names, colnames(data))
 tg_filtered    <- intersect(target_names, colnames(data))
 
@@ -35,45 +45,10 @@ q975           <- quantile(as.numeric(data), probs = 0.975 , na.rm = FALSE)
 data           <- data/q975
 data           <- replace(data, data>=1, 0.9999)
 data[1:10,1:10]
-################################################################################
-# load base grns for each tissue
-if(tissue=="Spleen"){
-  base_grn     <- atac_candidates_peak[[1]]
-}else if(tissue=="Lung"){
-  base_grn     <- atac_candidates_peak[[2]]
-}else if(tissue=="Liver"){
-  base_grn     <- atac_candidates_peak[[3]]
-}else if(tissue=="Kidney"){
-  base_grn     <- atac_candidates_peak[[4]]
-}else if(tissue=="Heart_and_Aorta"){
-  base_grn     <- atac_candidates_peak[[5]]
-}
-############################################################################ identify TF-gene network
-source("scGATE_edge.R")
+################################################################################ identify TF-gene network
 abs_cor        <- 0
 k_act          <- 0.7
 h_act          <- 7
-res            <-  scGATE_edge(data = data, number_of_em_iterations = 3, max_num_regulators = 3, k_act = k_act, h_act = h_act, tf_names=tf_filtered, tg_names=tg_filtered, abs_cor, base_grn)
-##############################################################################
-source("label_calc_beeline.R")
-TF_TARGET             <- as.data.frame(read.csv(paste0("D:\\scRNA logic\\mouse spleen dataset\\BEELINE\\data_cor\\" , tissue_of_interest, "\\", "refNetwork.csv"), header = TRUE))
-TF_TARGET             <- TF_TARGET[TF_TARGET$Gene1 != TF_TARGET$Gene2, ]
-
-ranked_edge_list      <- res$ranked_edge_list
-ranked_edge_list$label<- 0
-
-ranked_edge_list      <- label_calc(tf_numbers=length(tf_filtered), tg_numbers=length(tg_filtered), TF_TARGET, ranked_edge_list)
-ranked_list           <- ranked_edge_list$BF_score
-labels                <- ranked_edge_list$label
-pred                  <- prediction(ranked_list, labels)
-auc                   <- performance(pred, measure = "auc")@y.values[[1]]
-##############################################################################
-ranked_edge_list$label<- as.numeric(ranked_edge_list$label)
-edge_numbers          <- sum(ranked_edge_list$label)
-EPR                   <- sum(ranked_edge_list[1:edge_numbers, 4])
-##############################################################################
-print(c(tissue_of_interest, auc, EPR, EPR/edge_numbers))
-
-
-
-
+res            <-  scGATE_edge(data = data, number_of_em_iterations = 3, max_num_regulators = 3, k_act = k_act, h_act = h_act, tf_names=tf_filtered, tg_names=tg_filtered, base_grn, abs_cor)
+print(res$ranked_edge_list)
+################################################################################
