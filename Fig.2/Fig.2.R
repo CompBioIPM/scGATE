043f4da5318d1c5bfcb87615e83f0653f6b7813a
CompBioIPM <47293318+CompBioIPM@users.noreply.github.com>
2023-12-02 17:05:45 +0330

    Update Fig.2.R

--- Fig.2/Fig.2.R
+++ Fig.2/Fig.2.R
@@ -1,65 +1,22 @@
-
+# Context-specific network and logic gate inference in synthetic toggle switch
+# 1. Please refer to the Jupyter notebook for instructions on how to perform Louvain clustering on the cells in the BoolODE simulated data.
+# 2. Retrieve the data from Cluster I of cells, which was obtained in the previous step.
+# Load scGATE package and data in example_data folder
+ 
 rm(list = ls())
 library(scGATE)
-################################################################################
-# load data
-ClusterI  <- as.data.frame(read.csv(paste0("\\write_fig2\\ClusterI.csv"))[ ,2:15])
-ClusterII <- as.data.frame(read.csv(paste0("\\write_fig2\\ClusterII.csv"))[ ,2:15])
-data      <- ClusterII
-################################################################################
-# data normalization
-q975      <- quantile(c(as.matrix(data)), probs = 0.975 , na.rm = FALSE)
-data      <- data/q975
-data      <- replace(data, data>=1, 0.9999)
-################################################################################
-# remove genes with low variability
-data      <- data[ , which(sqrt(apply(data,2,var))> 0.20)]
-################################################################################
-# base grn definition
-gene_list <- colnames(data)
-base_grn  <- c()
-for(i in 1:length(gene_list)){
-  for(j in 1:length(gene_list)){
-    if(i!=j){
-      base_grn  <- rbind(base_grn, c(gene_list[j], gene_list[i]))
-    }
-  }
-}
-################################################################################
-# Please note that the likelihood values can be affected by the tSNE clustering results and the number of EM iterations.
+data         <- as.matrix(read.csv(paste0("/example_data/ClusterI.csv"))[ ,2:15])
 
-h_set     <- c(1, 1.25, 1.75, 2.5, 5, 7) # the range of h values in the Hill climbing function
-gates     <- scGATE_logic(data = data, number_of_em_iterations = 5, max_num_regulators = 3, h_set = h_set, base_grn = base_grn, abs_cor = 0.05, top_gates = 1, grid_par = "simple")
-print(gates)
-################################################################################
+# 3. data preprocessing 
+# For scGATE simulated data, library size normalization is not performed. 
+# However, the simulated data is only re-scaled using the quantile normalization technique to fit the data within the (0,1) interval.
+data         <- scRNA_seq_preprocessing(data = data, library_size_normalization = "False")
 
+# 4. Remove genes with low variability (scGATE operates on highly variable genes per context).
+# This step is optional
+data$n_counts<- data$n_counts[ , which(sqrt(apply(data$n_counts,2,var))> 0.20)]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 5. Run scGATE_logic() function
+# Please note that the likelihood values can be affected by the Louvain clustering results.
+gates        <- scGATE_logic(data = data, top_gates = 1, run_mode = "fast")
+print(head(gates))
